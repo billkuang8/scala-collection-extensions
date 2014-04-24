0d28715a2ed9e4ac9db0522e24df824903a8bab5
Bill Kuang <bkuang@bizo.com>
2014-04-24 11:39:29 -0700

    remove printlns from ScalaCollectionExtensionTest.scala

--- src/test/scala/com/github/billkuang/collection/ScalaCollectionExtensionTest.scala
+++ src/test/scala/com/github/billkuang/collection/ScalaCollectionExtensionTest.scala
@@ -21,12 +21,10 @@ class ExtensionsTest extends Matchers {
   @Test
   def sampleTest {
     val sampleWithReplacement = Seq(1, 2, 3, 4, 5, 6, 7).sample(withReplacement = true, sampleSize = 5)
-    sampleWithReplacement foreach println
     sampleWithReplacement should have size (5)
     sampleWithReplacement.distinct.size should be <= 5
 
     val sampleWithoutReplacement = Seq(1, 2, 3, 4, 5, 6, 7).sample(withReplacement = false, sampleSize = 5)
-    sampleWithoutReplacement foreach println
     sampleWithoutReplacement should have size (5)
     sampleWithoutReplacement.distinct.size should be (5)
   }
